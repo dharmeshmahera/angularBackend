54e520fd1589c879bf59a4a5a693057700d65a76
dharmeshmahera <92814870+dharmeshmahera@users.noreply.github.com>
2021-10-20 00:51:16 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "root@123",
+    password: "",
     database: "dharmeshdb"
 
 });
