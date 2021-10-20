268ef4e18a85c637f86e72f7aa6d1c7b8f370007
dharmesh.mehera <dharmesh.mahera@theonetechnologies.co.in>
2021-10-21 01:06:50 +0530

    final

--- connection.js
+++ connection.js
@@ -1,15 +1,24 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root@123",
-    database: "dharmeshdb"
-
+    host: "156.67.222.85",
+    user: "u110886746_otp",
+    password: "Hitesh@1234",
+    database: "u110886746_sms"
 });
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "root@123",
+//     database: "dharmeshdb",
 
+// });
 connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Connected!");    
+    }
+    
 });
 
 global.con = connection;
